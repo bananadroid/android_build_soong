333e34eece7c734010c116077b5d9c4bbdd87b4c
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-03 00:11:47 +0900

    soong: Do not use compression upon creating zip (jar) files
    A lot of zip files are created during builds for intermediate purposes
    and not using compression significantly shortens build times.
    
    Making these uncompressed allows the host OS to utilize page-cache too.
    
    Note that this change makes the zip files under out/dist/ to also be
    uncompressed files, but it won't matter much as the ota zip file's contents
    are compressed on its own.
    
    Change-Id: I6e0a96563468e84fce148e63e4658922542b6c26
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- zip/zip.go
+++ zip/zip.go
@@ -417,10 +417,7 @@ func fillPathPairs(fa FileArg, src string, pathMappings *[]pathMapping,
 	}
 	dest = filepath.Join(fa.PathPrefixInZip, dest)
 
-	zipMethod := zip.Deflate
-	if _, found := nonDeflatedFiles[dest]; found || noCompression {
-		zipMethod = zip.Store
-	}
+	zipMethod := zip.Store
 	*pathMappings = append(*pathMappings,
 		pathMapping{dest: dest, src: src, zipMethod: zipMethod})
 
@@ -465,7 +462,7 @@ func (z *ZipWriter) write(f io.Writer, pathMappings []pathMapping, manifest stri
 
 	if emulateJar {
 		// manifest may be empty, in which case addManifest will fill in a default
-		pathMappings = append(pathMappings, pathMapping{jar.ManifestFile, manifest, zip.Deflate})
+		pathMappings = append(pathMappings, pathMapping{jar.ManifestFile, manifest, zip.Store})
 
 		jarSort(pathMappings)
 	}
