91347565802154972a1f613812c862a34a0b6029
johnmart19 <johnivan19999@gmail.com>
2021-11-14 21:47:30 +0200

    soong: Allow Flex, Bison and M4 to be used
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -87,6 +87,7 @@ func GetConfig(name string) PathConfig {
 // run during the build. For more documentation, see path_interposer.go .
 var Configuration = map[string]PathConfig{
 	"bash":    Allowed,
+	"bison":   Allowed,
 	"brotli":  Allowed,
 	"ccache":  Allowed,
 	"cpio":    Allowed,
@@ -94,6 +95,7 @@ var Configuration = map[string]PathConfig{
 	"diff":    Allowed,
 	"dlv":     Allowed,
 	"expr":    Allowed,
+	"flex":    Allowed,
 	"fuser":   Allowed,
 	"getopt":  Allowed,
 	"git":     Allowed,
@@ -103,6 +105,7 @@ var Configuration = map[string]PathConfig{
 	"javap":   Allowed,
 	"locale":  Allowed,
 	"lsof":    Allowed,
+	"m4":      Allowed,
 	"nproc":   Allowed,
 	"openssl": Allowed,
 	"pstree":  Allowed,
