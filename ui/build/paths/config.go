2369da384b0fd17ecf0f5d30fe73948232a39dea
Vivekachooz <vivekachooz@gmail.com>
2022-02-25 12:17:23 +0530

    soong: Allow aarch64-linux-gnu-{as,ld}
    Change-Id: Ieedf9d3f9f495d5bd20b123c81d03e7fabf54625
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -99,6 +99,8 @@ var Configuration = map[string]PathConfig{
 	"tr":      Allowed,
 	"unzip":   Allowed,
 	"zip":     Allowed,
+	"aarch64-linux-gnu-as": Allowed,
+	"aarch64-linux-gnu-ld": Allowed,
 	"arm-linux-androidkernel-as": Allowed,
 	"arm-linux-androidkernel-ld": Allowed,
 
