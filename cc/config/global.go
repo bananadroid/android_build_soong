5d5541034a3fe45003a8c6c042702822568dff71
Chih-hung Hsieh <chh@google.com>
2022-09-27 07:19:03 +0000

    Reland "Update clang version to clang-r468909"
    This reverts commit fc21a0e55bac3ba4c73f7bdb2f963ec38a8903c4.
    Reason for revert: broken builds/tests were fixed
    
    Bug: 241011085
    Change-Id: Ifba0bb05e690c8204e3ef34f59d7bf7ef57638ed

--- cc/config/global.go
+++ cc/config/global.go
@@ -234,6 +234,12 @@ var (
 		// New warnings to be fixed after clang-r433403
 		"-Wno-error=unused-but-set-variable",  // http://b/197240255
 		"-Wno-error=unused-but-set-parameter", // http://b/197240255
+		// New warnings to be fixed after clang-r458507
+		"-Wno-error=unqualified-std-cast-call", // http://b/239662094
+		// New warnings to be fixed after clang-r468909
+		"-Wno-error=array-parameter",     // http://b/241941550
+		"-Wno-error=deprecated-builtins", // http://b/241601211
+		"-Wno-error=deprecated",          // in external/googletest/googletest
 	}
 
 	noOverrideExternalGlobalCflags = []string{
@@ -291,8 +297,8 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r450784d"
-	ClangDefaultShortVersion = "14.0.6"
+	ClangDefaultVersion      = "clang-r468909"
+	ClangDefaultShortVersion = "15.0.2"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
