2e3c5a499f650d13276a5765dec0eb30b74c4aa2
Rizky Benggolo <travarilo@gmail.com>
2023-04-08 22:51:45 +0200

    Revert "Use the current java version for -jvm-target"
    This reverts commit 3dcaf7550f44ae83c3b5f2da31d7d4b9e5a86b10.

--- java/kotlin.go
+++ java/kotlin.go
@@ -119,8 +119,9 @@ func kotlinCompile(ctx android.ModuleContext, outputFile, headerOutputFile andro
 			"srcJarDir":         android.PathForModuleOut(ctx, "kotlinc", "srcJars").String(),
 			"kotlinBuildFile":   android.PathForModuleOut(ctx, "kotlinc-build.xml").String(),
 			"emptyDir":          android.PathForModuleOut(ctx, "kotlinc", "empty").String(),
-			"kotlinJvmTarget":   flags.javaVersion.StringForKotlinc(),
-			"name":              kotlinName,
+			// http://b/69160377 kotlinc only supports -jvm-target 1.6 and 1.8
+			"kotlinJvmTarget": "1.8",
+			"name":            kotlinName,
 		},
 	})
 }
