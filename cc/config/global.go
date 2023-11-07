d25d60af63603f312e53c6b54a00e18ef4513001
Yi Kong <yikong@google.com>
2023-11-07 14:12:51 +0900

    Only enable MLGO for ARM64 ThinLTO targets
    The MLGO model we embed in the Android Clang is trained on ARM64
    ThinLTO artefacts. Applying the model across architectures does not
    necessarily translate to performance improvement. Therefore only enable
    the optimisation on ARM64 ThinLTO for now.
    
    Test: presubmit
    Bug: 293827654
    Change-Id: Ie64a65c11191cf700a463637a0746c8470d3aa3c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- cc/config/global.go
+++ cc/config/global.go
@@ -141,9 +141,6 @@ var (
 		"-Werror=format-security",
 		"-nostdlibinc",
 
-		// Enable MLGO for register allocation.
-		"-mllvm -regalloc-enable-advisor=release",
-
 		// Emit additional debug info for AutoFDO
 		"-fdebug-info-for-profiling",
 
@@ -173,8 +170,6 @@ var (
 		"-Wl,--exclude-libs,libgcc_stripped.a",
 		"-Wl,--exclude-libs,libunwind_llvm.a",
 		"-Wl,--exclude-libs,libunwind.a",
-		// Enable MLGO for register allocation.
-		"-Wl,-mllvm,-regalloc-enable-advisor=release",
 	}
 
 	deviceGlobalLldflags = append(deviceGlobalLdflags, commonGlobalLldflags...)
